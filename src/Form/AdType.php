8a18fdbacad873d314dfed09add76536a5bbd92b
Julienept <lefebvjulie@gmail.com>
2019-06-17 14:42:01 +0200

    formulaire dépôt d'annonce

--- src/Form/AdType.php
+++ src/Form/AdType.php
@@ -6,6 +6,10 @@ use App\Entity\Ad;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
@@ -14,15 +18,31 @@ class AdType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextareaType::class)
-            ->add('description', TextareaType::class)
-            ->add('price', NumberType::class)
-            ->add('date')
-            ->add('privateTransport')
-            ->add('publicTransport')
-            ->add('atHome')
-            ->add('atLaundryService')
-            ->add('atFriendsPlace')
+            ->add('title', TextType::class, [
+                'label' => 'Titre de l\'annonce'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description de l\'annonce'
+            ])
+            ->add('price', NumberType::class, [
+                'label' => 'Votre tarif'
+            ]) 
+            ->add('place', TextareaType::class, [
+                'label' => 'Ville où se déroulera la prestation'
+            ])
+            ->add('privateTransport', RadioType::class, [
+                'label' => 'Votre véhicule personnel'
+            ])
+            ->add('publicTransport', RadioType::class, [
+                'label' => 'En transport en commun'
+            ])
+            ->add('atHome', RadioType::class, [
+                'label' => 'A votre domicile',
+                'required' => 'true', 
+
+                ])
+            ->add('atLaundryService', RadioType::class)
+            ->add('atFriendsPlace', RadioType::class)
             ->add('gentleHouseholdProduct')
             ->add('hospitalProduct')
             ->add('earthProtection')
@@ -32,7 +52,6 @@ class AdType extends AbstractType
             ->add('tumbleDryer')
             ->add('airDrying')
             ->add('ironing')
-            ->add('place')
             ->add('tags')
         ;
     }
